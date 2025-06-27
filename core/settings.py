c36bd5b70693d09bff902e8be2fc37c1a3e7fb97
BenjaminMIG3 <122969282+BenjaminMIG3@users.noreply.github.com>
2025-06-28 00:54:18 +0200

    Load SECRET_KEY and DEBUG from environment variables
    Updated settings to retrieve SECRET_KEY and DEBUG from environment variables instead of hardcoding them. This improves security and flexibility for different deployment environments.

--- core/settings.py
+++ core/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!^1+rn!%*(zz=%2$+*)!=6__bd$t7hk2x(g)c%h1_4m_i^6s-#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['192.168.1.66', 'chatforall.online', 'www.chatforall.online', 'localhost', '127.0.0.1', '192.168.1.179']
 
