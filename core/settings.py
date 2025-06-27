51430607c96f53d47123aa488620bd1e82b19f42
BenjaminMIG3 <122969282+BenjaminMIG3@users.noreply.github.com>
2025-06-28 00:56:57 +0200

    Fix DEBUG setting to default to False
    Updated the DEBUG setting to default to False unless the environment variable is explicitly set to 'True'. This ensures safer defaults in production environments.

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['192.168.1.66', 'chatforall.online', 'www.chatforall.online', 'localhost', '127.0.0.1', '192.168.1.179']
 
